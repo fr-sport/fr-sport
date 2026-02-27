14d9fe327fdc86ff0f1554f98cfe996c1d230680
fr-sport <farhad10180@gmail.com>
2026-02-27 20:14:45 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'fr-sport-v1';
+const urlsToCache = [
+  '/fr-sport/',
+  '/fr-sport/index.html',
+  '/fr-sport/style.css',
+  '/fr-sport/script.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
