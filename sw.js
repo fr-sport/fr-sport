9d034342da5d09eade22b502ef7e77593707de0d
fr-sport <farhad10180@gmail.com>
2026-03-01 03:01:13 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,11 @@
-const CACHE_NAME = 'fr-sport-cache-v1';
+const CACHE_NAME = 'fr-sport-cache-v2';
 const urlsToCache = [
   '/fr-sport/',
   '/fr-sport/index.html',
-  '/fr-sport/Logo.jpg'
+  '/fr-sport/manifest.json',
+  '/fr-sport/android-chrome-192x192.png',
+  '/fr-sport/android-chrome-512x512.png',
+  '/fr-sport/apple-touch-icon.png'
 ];
 
 self.addEventListener('install', event => {
@@ -16,5 +19,19 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
